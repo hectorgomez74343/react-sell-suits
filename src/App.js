6225fe8ed992883b9eab11b8a56b3235202ce7ea
hectorgomez74343 <hmg701@yahoo.com>
2021-06-07 23:20:54 -0500

    Updated src folder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, withRouter, Route } from "react-router-dom";
 
 import IndexPage from "./components/IndexPage";
+import News from "./components/News";
 
 class App extends React.PureComponent {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <IndexPage />} />
+        <Route exact path="/" render={() => <News />} />
+        <Route exact path="/info" render={() => <IndexPage />} />
       </Switch>
     );
   }
