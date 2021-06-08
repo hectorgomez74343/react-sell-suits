e93c404c5c261fdf861bc4627d24d37036eafef9
hectorgomez74343 <hmg701@yahoo.com>
2021-06-07 23:04:10 -0500

    Migrated from create-react-app to webpack

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Switch, withRouter, Route } from "react-router-dom";
 
 import IndexPage from "./components/IndexPage";
-import News from "./components/News";
 
 class App extends React.PureComponent {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <News />} />
-        <Route exact path="/info" render={() => <IndexPage />} />
+        <Route exact path="/" render={() => <IndexPage />} />
       </Switch>
     );
   }
--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,50 +1,260 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
-import Section from "./Section";
-import splashImg from "../splash.png";
+import "./Card.css";
 
 class IndexPage extends React.PureComponent {
+  state = {
+    turn: "Blue",
+    cardOne: null,
+    cardTwo: null,
+    cardThree: null,
+    cardFour: null,
+    cardFive: null,
+    cardSix: null,
+    cardSeven: null,
+    cardEight: null,
+    cardNine: null,
+  };
+
+  startOver = () => {
+    this.setState({
+      turn: "Blue",
+      cardOne: null,
+      cardTwo: null,
+      cardThree: null,
+      cardFour: null,
+      cardFive: null,
+      cardSix: null,
+      cardSeven: null,
+      cardEight: null,
+      cardNine: null,
+    });
+  };
+
+  setCardColor = (cardNumber) => {
+    const { turn } = this.state;
+
+    if (cardNumber === "one") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardOne: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardOne: "#DF740C",
+        });
+        return null;
+      }
+    }
+    if (cardNumber === "two") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardTwo: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardTwo: "#DF740C",
+        });
+        return null;
+      }
+    }
+    if (cardNumber === "three") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardThree: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardThree: "#DF740C",
+        });
+        return null;
+      }
+    }
+    if (cardNumber === "four") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardFour: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardFour: "#DF740C",
+        });
+        return null;
+      }
+    }
+    if (cardNumber === "five") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardFive: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardFive: "#DF740C",
+        });
+        return null;
+      }
+    }
+    if (cardNumber === "six") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardSix: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardSix: "#DF740C",
+        });
+        return null;
+      }
+    }
+    if (cardNumber === "seven") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardSeven: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardSeven: "#DF740C",
+        });
+        return null;
+      }
+    }
+    if (cardNumber === "eigth") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardEight: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardEight: "#DF740C",
+        });
+        return null;
+      }
+    }
+    if (cardNumber === "nine") {
+      if (turn === "Blue") {
+        this.setState({
+          turn: "Red",
+          cardNine: "#0ef8f8",
+        });
+        return null;
+      }
+      if (turn === "Red") {
+        this.setState({
+          turn: "Blue",
+          cardNine: "#DF740C",
+        });
+        return null;
+      }
+    }
+  };
   render() {
+    const {
+      cardOne,
+      cardTwo,
+      cardThree,
+      cardFour,
+      cardFive,
+      cardSix,
+      cardSeven,
+      cardEight,
+      cardNine,
+    } = this.state;
+
     return (
-      <div>
-        <div className="Hero">
-          <div className="HeroGroup">
-            <h1>
-              News App <br />
-              International
-            </h1>
-            <p>The future of news is here</p>
-          </div>
+      <div className="Cards">
+        <h2>3D Tic Tac Toe</h2>
+        <div className="start-over-flex-box ">
+          {" "}
+          <button className="start-over-button" onClick={this.startOver}>
+            Start Over
+          </button>
         </div>
 
-        <div id="contact">
-          <Section
-            height="500"
-            image={splashImg}
-            title="About"
-            text="The technologies used in this website are HTML-5, CSS3, JavaScript, JSX, React JS, Redux, Node JS, Mongo DB, Express JS, Heroku, and Netlify. To get a copy of this codebase scroll down to latest news."
-          />
-        </div>
-        <div id="contact">
-          <Section
-            height="500"
-            image={splashImg}
-            title="Privacy"
-            text="News App International does not collect, sell, or store data."
-          />
-        </div>
-        <div id="contact">
-          <Section
-            height="500"
-            image={splashImg}
-            title="Latest News"
-            text="We are pleased to announce that you can look at the CSS code and replicate this idea/invention for free, just get a good coder tell him to look at the Card class. You can get all the free code you want in this github repo including examples of this code to sell items, is millions of dollars in clean bugless regression free code "
-            url="https://github.com/hectorgomez74343?tab=repositories"
-          />
+        <div className="Card-Flex-Box">
+          <div className="CardGroup">
+            <div
+              onClick={this.setCardColor.bind(this, "one")}
+              style={{ backgroundColor: cardOne }}
+              className="Card"
+            ></div>
+            <div
+              onClick={this.setCardColor.bind(this, "two")}
+              style={{ backgroundColor: cardTwo }}
+              className="Card"
+            ></div>
+            <div
+              onClick={this.setCardColor.bind(this, "three")}
+              style={{ backgroundColor: cardThree }}
+              className="Card"
+            ></div>
+            <div
+              onClick={this.setCardColor.bind(this, "four")}
+              style={{ backgroundColor: cardFour }}
+              className="Card"
+            ></div>
+            <div
+              onClick={this.setCardColor.bind(this, "five")}
+              style={{ backgroundColor: cardFive }}
+              className="Card"
+            ></div>
+            <div
+              onClick={this.setCardColor.bind(this, "six")}
+              style={{ backgroundColor: cardSix }}
+              className="Card"
+            ></div>
+            <div
+              onClick={this.setCardColor.bind(this, "seven")}
+              style={{ backgroundColor: cardSeven }}
+              className="Card"
+            ></div>
+            <div
+              onClick={this.setCardColor.bind(this, "eigth")}
+              style={{ backgroundColor: cardEight }}
+              className="Card"
+            ></div>
+            <div
+              onClick={this.setCardColor.bind(this, "nine")}
+              style={{ backgroundColor: cardNine }}
+              className="Card"
+            ></div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default IndexPage;
+export default withRouter(IndexPage);
